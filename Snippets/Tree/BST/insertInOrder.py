20f5e2321445727b164b66869a164605fb000a45
dhairyashilp <dh.patil09@gmail.com>
2021-02-04 07:57:12 -0500

    4 Feb 2021

---
+++ Snippets/Tree/BST/insertInOrder.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def insert(root,val):
+    if not root:
+        return TreeNode(val)
+    if root.val>val:
+        root.left=insert(root.left,val)
+    else:
+        root.right=insert(root.right,val)
+    return root
