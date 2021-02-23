3fd3f446f9550e81302b90e44c101399a64dd07f
dhairyashilp <dh.patil09@gmail.com>
2021-02-22 21:30:02 -0500

    22 Feb 2021

---
+++ LeetCode/933/solution.py
@@ -0,0 +1,15 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.calls = deque()
+
+    def ping(self, t: int) -> int:
+        self.calls.append(t)
+        while self.calls[0] < self.calls[-1] - 3000:
+            self.calls.popleft()
+        return len(self.calls)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
