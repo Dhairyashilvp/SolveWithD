fbb4197a979407b5709be35e0fe5163b5a933d65
dhairyashilp <dh.patil09@gmail.com>
2021-02-23 22:53:23 -0500

    23 Feb 2021

---
+++ LeetCode/728/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        ans = []
+        for i in range(left,right+1):
+            for n in str(i):
+                if n == "0" or i % int(n) != 0:
+                    break
+            else:
+                ans.append(i)
+        return ans
+                    
