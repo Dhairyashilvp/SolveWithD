4c2e955a7de6e564de3bb7706d1a2c482326653a
dhairyashilp <dh.patil09@gmail.com>
2021-02-24 20:50:51 -0500

    24 Feb 2021

---
+++ LeetCode/1431/solution.py
@@ -0,0 +1,3 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        return [True if c + extraCandies >= max(candies) else False for c in candies]
